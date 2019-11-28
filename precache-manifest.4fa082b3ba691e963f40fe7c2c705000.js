ffdf857ba0fbb8709507e54a0c2e1753f0988c03
inzahan <inzahanyang@gmail.com>
2019-11-28 16:02:29 +0900

    Updates

---
+++ precache-manifest.4fa082b3ba691e963f40fe7c2c705000.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d78b2d353a29f119ce748f64528b40",
+    "url": "/movie2019/index.html"
+  },
+  {
+    "revision": "abb13a01920b1e0540bb",
+    "url": "/movie2019/static/js/2.53c948a6.chunk.js"
+  },
+  {
+    "revision": "ac2a459fa9c5137847d2",
+    "url": "/movie2019/static/js/main.b0bd82e2.chunk.js"
+  },
+  {
+    "revision": "97b8f80cb5190ecec578",
+    "url": "/movie2019/static/js/runtime-main.85cf3587.js"
+  }
+]);
